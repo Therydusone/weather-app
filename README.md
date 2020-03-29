c960be4bf67c1f9e7fc909acf2f2028d7eb7ad72
Therydusone <przem.pajak@gmail.com>
2020-03-29 19:20:54 +0200

    added Celsius to Fahrenheit formula and readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weather-app
 Simple weather app in VanillaJS to train basic skills
+
+Used Dark Sky api to take weather depending on my location:
+https://darksky.net/
+
+Icons are animated from:
+https://darkskyapp.github.io/skycons/
+
+
+<img src="Temperatura.jpg" style="width:300px;height:300px;">
--- app.js
+++ app.js
@@ -4,7 +4,9 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone')
-
+    let temperatureSection = document.querySelector('.degree-section')
+    const temperatureSpan = document.querySelector('.degree-section span')
+    
     if(navigator.geolocation){
        navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
@@ -23,8 +25,23 @@ window.addEventListener('load', () => {
             temperatureDegree.textContent = temperature
             temperatureDescription.textContent = summary
             locationTimezone.textContent = data.timezone;
+            //Formula for celsius
+            let celsius = (temperature - 32) * (5 / 9);
+           
+           
             //Set Icon
-            setIcons(icon, document.querySelector(".icon"))
+            setIcons(icon, document.querySelector(".icon"));
+
+            //Change temperature to Celsius/Fahrenheit
+            temperatureSection.addEventListener('click', () => {
+                if(temperatureSpan.textContent ==="F"){
+                    temperatureSpan.textContent = "C"
+                    temperatureDegree.textContent = Math.floor(celsius);
+                } else {
+                    temperatureSpan.textContent = "F";
+                    temperatureDegree.textContent = temperature;
+                }
+            })
         });
     });
     }
